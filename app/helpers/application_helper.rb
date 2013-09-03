0e7430eb349ff321197329ea66c22b6768f86d2e
NamelessOne <unfortunate42@gmail.com>
2013-09-04 00:13:09 +0200

    Implemented item format.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_published_at(datetime)
+    if datetime > Time.now.beginning_of_day
+      datetime.strftime("#{t(:today)} %k:%M")
+    elsif datetime > 1.day.ago.beginning_of_day
+      datetime.strftime("#{t(:yesterday)} %k:%M")
+    else
+      datetime.to_s(:short)
+    end
+  end
 end
--- config/initializers/xml_schema.rb
+++ config/initializers/xml_schema.rb
@@ -5,11 +5,15 @@ class Item
   xml_reader :type_of_media
   xml_reader :language
   xml_reader :url
-  xml_reader :published_at
+  xml_reader :published_at, as: DateTime
 
   xml_reader :header
   xml_reader :summary
+  xml_reader :body
+  xml_reader :boilerplate
+  xml_reader :image_thumbnail_large
   xml_reader :image_thumbnail_medium
+  xml_reader :image_thumbnail_small
 end
 
 class Feed
