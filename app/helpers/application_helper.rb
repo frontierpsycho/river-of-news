1bfc86900d86ada4f6d98393e6bb5b87fdb0e508
NamelessOne <unfortunate42@gmail.com>
2013-09-04 16:10:03 +0200

    Fixed UTC bug in display_published_at.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def display_published_at(datetime)
-    if datetime > Time.now.beginning_of_day
+    if datetime > Time.now.utc.beginning_of_day
       datetime.strftime("#{t(:today)} %k:%M")
-    elsif datetime > 1.day.ago.beginning_of_day
+    elsif datetime > 1.day.ago.utc.beginning_of_day
       datetime.strftime("#{t(:yesterday)} %k:%M")
     else
       datetime.to_s(:short)
